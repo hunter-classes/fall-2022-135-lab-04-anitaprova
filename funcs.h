60f591b60c716d5af0c7da6402928e19566cbbb6
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-17 22:37:51 -0400

    almost done

---
+++ funcs.h
@@ -0,0 +1,13 @@
+#include <string>
+#ifndef FUNCS_H_
+#define FUNCS_H_
+
+std::string box (int width, int height);
+std::string checkerboard (int width, int height);
+std::string cross (int size);
+std::string lower (int length);
+std::string upper (int length);
+std::string trapezoid(int width, int height);
+std::string checkerboard3x3(int width, int height);
+
+#endif
