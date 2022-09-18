dc96f55493d12c91d5166403a9652e11679324da
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-17 22:38:30 -0400

    almost done

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include "funcs.h"
+
+int main()
+{
+	std::string result;
+
+	std::cout << "box(7, 4):\n";
+	result = box(7, 4);
+	std::cout << result;
+	std::cout << "\n------------------\n" << std::endl;;
+
+	std::cout << "checkerboard(11, 6)\n";
+	result = checkerboard(11, 6);
+	std::cout << result;
+	std::cout << "\n------------------\n" << std::endl;
+
+	std::cout << "cross(10):\n";
+	result = cross(10);
+	std::cout << result;
+	std::cout << "\n------------------\n" << std::endl;
+	
+	std::cout << "lower(6)\n";
+	result = lower(6);
+	std::cout << result;
+	std::cout << "\n------------------\n" << std::endl;
+
+	std::cout << "upper(6)\n";
+	result = upper(6);
+	std::cout << result;
+	std::cout << "\n------------------\n" << std::endl;
+	
+	std::cout << "trapezoid(12, 5)\n";
+	result = trapezoid(12, 5);
+	std::cout << result;
+	std::cout << "\n------------------\n" << std::endl;
+
+	std::cout << "checkerboard3x3(16,11):\n";
+	result = checkerboard3x3(16,11);
+	std::cout << result;
+	std::cout << "\n------------------\n" << std::endl;
+}
