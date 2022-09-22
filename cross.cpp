e41fb100053c8b0196aab56b4dff8364bd0bc791
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-22 12:39:20 -0400

    Create cross.cpp

---
+++ cross.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "cross.h"
+
+std:: string cross(int size)
+{
+	std::cout << "Shape: \n";
+	int b = 0;
+	int e = size - 2;
+
+	for (int row = 0; row < size/2; row++) {
+		for (int i = 0; i < b; i++) {
+			std::cout << " ";
+		}
+		b++;	
+		std::cout << "*";
+		
+		for(int i = e; i > 0; i--) {
+			std::cout << " ";
+		}
+		e -= 2;
+		std::cout << "*\n";	
+	}
